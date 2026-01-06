eaeed088ba39d0a63672a780f699125c1f4ac8d9
Guilherme Luz <161989907+Devsantosz@users.noreply.github.com>
2026-01-06 15:05:59 -0300

    python

--- test/flower/app.py
+++ test/flower/app.py
@@ -6,14 +6,13 @@ janela.title("FLOWERS")
 janela.geometry("400x400")
 janela.configure(bg="#57006d")
 
-
 container = tk.Frame(
     janela,
     width=300,
     height=200,
     bg="#d60707"
 )
-container.pack(expand=True)
+container.place(relx=0.5, rely=0.5, anchor="center")
 
 tk.Label(container, text="Usuário").grid(row=0, column=0, sticky="e")
 tk.Entry(container).grid(row=0, column=1)

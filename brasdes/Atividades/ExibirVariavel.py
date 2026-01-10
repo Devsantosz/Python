10609fddacdf0564ff30dd2d9ed583ce2f981ece
Guilherme Luz <161989907+Devsantosz@users.noreply.github.com>
2026-01-10 17:35:26 -0300

    Aplicação de conhecimentos e estudo de Python
    Fundação Bradesco

---
+++ brasdes/Atividades/ExibirVariavel.py
@@ -0,0 +1,9 @@
+codigo = int(input("Digite o codigo do funcionario"))
+nome = input("Digite o nome do funcionario")
+salario = float(input("Digite o salario do funcionario"))
+ativo = True
+
+print("Código: %d "% codigo)
+print("Nome: %s "% nome)
+print("Salario: %s "% salario)
+print("Ativo: %s "% ativo)
---
+++ brasdes/Atividades/TipoVariavel.py
@@ -0,0 +1,11 @@
+codigo = 10
+salario = 1500.00
+nome = 'Jose'
+situacao = True
+
+tipo = type(salario)
+
+print(salario)
+print(tipo)
+
+print("Código:"+ str (codigo)+ "Nome:"+ str (nome)+ "O salario atual é de" + str ( salario))

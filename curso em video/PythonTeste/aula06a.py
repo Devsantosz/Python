3aa2a2f8a1e50677950cbcae7e4c0f921776ffe7
Guilherme Luz <161989907+Devsantosz@users.noreply.github.com>
2026-03-26 18:08:53 -0300

    Aula 06 python

---
+++ curso em video/PythonTeste/aula06a.py
@@ -0,0 +1,6 @@
+num01 = int(input('Digite o primeiro numero: '))
+num02 = int(input('Digite o segundo numero: '))
+s = num01 + num02
+
+print('A soma entre', num01, 'e', num02, 'deu:', s)
+print('A soma entre {0} e {1} vale {2}'.format(num01, num02, s))

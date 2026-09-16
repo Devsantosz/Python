a2e8bf3ef0ce38f59120c002437bca561d8ed483
Guilherme Luz <161989907+Devsantosz@users.noreply.github.com>
2026-09-16 19:15:17 -0300

    Pergunta se quer fazer login

--- workspace/log.py
+++ workspace/log.py
@@ -5,16 +5,21 @@ login_user = {"nome": "",
               "senha": ""
               }
 
-nome = input('Digite seu nome: ')
-senha = input('Digite sua senha: ')
+#Pergunta se deseja realizar o login
+tem_login = str(input('Deseja realizar o login? [S/N] ')).upper().strip()
 
-login_user["nome"] = nome
-login_user["senha"] = senha
 
-print('Login realizado com sucesso!')
-
-if(login_user["nome"] == nome and login_user["senha"] == senha):
-    print('Acesso permitido!')
+if (tem_login == 'S'):
+    nome = input('Digite seu nome: ')
+    senha = input('Digite sua senha: ')
+    print('Login realizado com sucesso!')
+    if(login_user["nome"] == nome and login_user["senha"] == senha):
+        print('Acesso permitido!')
+    else:
+        print('Acesso negado!')
 else:
-    print('Acesso negado!')
+    print('Login não realizado!')
+
+
+
 

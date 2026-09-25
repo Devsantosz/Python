3285c5ccd8352655c421ecb7484d8c4ff21b9075
Guilherme Luz <161989907+Devsantosz@users.noreply.github.com>
2026-09-25 16:54:51 -0300

    biblioteca de tempo em python (time)

--- curso em video/PythonExercicios/ex28.py
+++ curso em video/PythonExercicios/ex28.py
@@ -1,9 +1,11 @@
 import random 
+from time import sleep #biblioteca de tempo
 
 numero_aleatorio = random.randint(0,5)
 
 num_select = int(input('Digite um numero de 0 a 5: '))
-
+print("Processando resposta...")
+sleep(2) # Faz o computador "dormir por () determinado."
 if (num_select == numero_aleatorio):
     print('Voce ganho, parabens!')
 else:
